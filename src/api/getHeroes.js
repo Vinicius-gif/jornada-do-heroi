5b0c3c527ba1c25f709e5044e7a5e86ab48175d5
Vinicius-gif <vfpv@hotmail.com>
2023-11-01 15:09:21 -0300

    consumindo api heroes

---
+++ src/api/getHeroes.js
@@ -0,0 +1,8 @@
+const getHeroes = async () => {
+  const response = await fetch('http://homologacao3.azapfy.com.br/api/ps/metahumans');
+  const data = await response.json();
+  
+  return data;
+};
+  
+export default getHeroes;
