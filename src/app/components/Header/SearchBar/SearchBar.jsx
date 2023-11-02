0852bf4bf6294d9c8ac58e4d5af791a20c2336e6
Vinicius <vfpv@hotmail.com>
2023-11-02 16:18:21 -0300

    Update SearchBar

--- src/app/components/Header/SearchBar/SearchBar.jsx
+++ src/app/components/Header/SearchBar/SearchBar.jsx
@@ -1,52 +1,27 @@
 'use client';
 
-import React, { useContext, useState } from 'react';
+import React from 'react';
 import SearchIcon from '@mui/icons-material/Search';
-import { HeroesContext } from '@/app/contexts/HeroesContext';
-import getHeroes from '@/api/getHeroes';
 
-const SearchBar = () => {
-  const [searchValue, setSearchValue] = useState('');
-  const { heroes, setHeroes } = useContext(HeroesContext);
-
-  const handleSearch = async (event) => {
-    event.preventDefault();
-    
-    if (searchValue !== '') {
-      const filteredHeroes = heroes.filter((hero) =>
-        hero.name.toLowerCase().includes(searchValue.toLowerCase())
-      );
-
-      setHeroes(filteredHeroes);
-    } else {
-      getHeroes().then((response) => {
-        setHeroes(response);
-      });
-    }
-
-    setSearchValue('');
-  };
-
+const SearchBar = ({searchValue, setSearchValue}) => {
+  
   return (
-    <form
-      action=""
-      className="flex bg-white max-w-md shadow-sm justify-between gap-3 pr-3 xl:w-full lg:w-full md:w-4/5 sm:w-1/2"
-      onSubmit={handleSearch}
-    >
+    <section className="flex bg-white max-w-md shadow-sm justify-between gap-3 pr-3 xl:w-full lg:w-full md:w-4/5 sm:w-1/2">
       <input
-        className="p-3 grow outline-none text-xs font-medium border-r border-gray-300 text-gray-800"
-        type="search"
-        placeholder="Buscar Herói"
+        name="search" 
+        className="p-3 grow outline-none text-lg font-medium border-r border-gray-300 text-gray-800 placeholder-gray-800"
+        type="text"
+        placeholder="Buscar Herói..."
         value={searchValue}
         onChange={(e) => setSearchValue(e.target.value)}
       />
       <button
-        type="submit"
+        type="button"
         className="bg-none border-none text-base flex items-center justify-center text-gray-800"
       >
         <SearchIcon color="action"/>
       </button>
-    </form>
+    </section>
   );
 };
 
