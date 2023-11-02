b7530a94318ade241185cfa70ff5e3bf99684e5e
Vinicius <vfpv@hotmail.com>
2023-11-01 21:16:23 -0300

    add Heroes provider

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import Header from './components/Header/Header';
 import ListHeroes from './components/ListHeroes/ListHeroes';
+import HeroesContextProvider from './contexts/HeroesContext';
 
 export default function Home() {
   return (
-    <>
+    <HeroesContextProvider>
       <Header/>
       <ListHeroes/>
-    </>
+    </HeroesContextProvider>
   );
 }
