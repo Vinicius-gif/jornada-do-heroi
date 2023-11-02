e7c41b898244405dd0b7abf736a043089b1a7d30
Vinicius <vfpv@hotmail.com>
2023-11-02 16:18:58 -0300

    Add heroes filter

--- src/app/components/ListHeroes/ListHeroes.jsx
+++ src/app/components/ListHeroes/ListHeroes.jsx
@@ -1,14 +1,15 @@
 'use client';
 
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import CardHero from './CardHero/CardHero';
 import { HeroesContext } from '@/app/contexts/HeroesContext';
 import getHeroes from '@/api/getHeroes';
+import Header from '../Header/Header';
 
 export default function ListHeroes() {
 
   const {heroes, setHeroes, setLoading} = useContext(HeroesContext);
-  //const [totalPower, settotalPower] = React.useState([]);
+  const [search, setSearch] = useState('');
 
   React.useEffect(() => {
     getHeroes().then((response) => {
@@ -17,11 +18,26 @@ export default function ListHeroes() {
     });
   }, [setHeroes, setLoading]);
 
+  const filteredHeroes = search.length > 0
+    ? heroes.filter(hero => hero.name.toLowerCase().includes(search.toLowerCase()))
+    : [];
+
   return (
-    <section className="grid p-12 gap-6 content-center justify-items-center lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1">
-      {heroes.map((hero) => (
-        <CardHero key={hero.id} name={hero.name} thumb={hero.images.lg} power={hero.powerstats.combat} />
-      ))}
-    </section>
+    <main>
+      <Header searchValue={search} setSearchValue={setSearch}/>
+      {search.length > 0 ? (
+        <section className="grid p-12 gap-6 content-center justify-items-center lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1">
+          {filteredHeroes.map((hero) =>  (
+            <CardHero key={hero.id} name={hero.name} thumb={hero.images.lg} power={hero.powerstats.combat} />
+          ))}
+        </section>
+      ) : (
+        <section className="grid p-12 gap-6 content-center justify-items-center lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1">
+          {heroes.map((hero) =>  (
+            <CardHero key={hero.id} name={hero.name} thumb={hero.images.lg} power={hero.powerstats.combat} />
+          ))}
+        </section>
+      )}
+    </main>
   );
 }
