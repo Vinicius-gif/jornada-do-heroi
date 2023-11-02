50483078a5ab5f8505003a1801e4c4de4f76bc23
Vinicius <vfpv@hotmail.com>
2023-11-02 16:17:37 -0300

    remove header

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,10 @@
-import Header from './components/Header/Header';
 import ListHeroes from './components/ListHeroes/ListHeroes';
 import HeroesContextProvider from './contexts/HeroesContext';
 
 export default function Home() {
   return (
     <HeroesContextProvider>
-      <Header/>
+      {/* <Header/> */}
       <ListHeroes/>
     </HeroesContextProvider>
   );
